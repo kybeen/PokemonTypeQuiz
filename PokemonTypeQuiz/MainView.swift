7d37a99bb5d72a125516ee2533655cc6916e7d19
kybeen <rei980627@gmail.com>
2023-11-09 16:33:35 +0900

    [setting] SnapKit 패키지 설치

---
+++ PokemonTypeQuiz/MainView.swift
@@ -0,0 +1,24 @@
+//
+//  MainView.swift
+//  PokemonTypeQuiz
+//
+//  Created by 김영빈 on 2023/11/09.
+//
+
+import UIKit
+
+final class MainView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    private func setupUI() {
+        
+    }
+}

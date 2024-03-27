703c335224c392dac87d89ad70bd346f3ab368b9
kybeen <rei980627@gmail.com>
2024-03-27 23:25:46 +0900

    [feat] PokemonAPIService 추가 - Data > Netrowk에 생성

---
+++ PokemonTypeQuiz/Sources/Data/Network/PokemonAPIService.swift
@@ -0,0 +1,11 @@
+//
+//  PokemonAPIService.swift
+//  PokemonTypeQuiz
+//
+//  Created by 김영빈 on 3/27/24.
+//
+
+final class PokemonAPIService {
+    static let shared = PokemonAPIService()
+    
+}

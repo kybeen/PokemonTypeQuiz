e3c5e27c92a2b27f3daf9ffc029ca75a3da3dca0
kybeen <rei980627@gmail.com>
2024-03-28 01:07:39 +0900

    [feat] PokemonInfo 모델 추가 - Domain > Model 에 추가

---
+++ PokemonTypeQuiz/Sources/Domain/Model/PokemonInfo.swift
@@ -0,0 +1,14 @@
+//
+//  PokemonInfo.swift
+//  PokemonTypeQuiz
+//
+//  Created by 김영빈 on 3/27/24.
+//
+
+struct PokemonInfo {
+    var id: Int
+    var name: String
+    var imageURL: String
+    var type1: PokemonType?
+    var type2: PokemonType?
+}

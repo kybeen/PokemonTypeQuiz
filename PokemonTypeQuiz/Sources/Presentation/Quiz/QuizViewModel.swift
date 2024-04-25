1c739f731137622de9f2d1f4dba6b2f8357a97c8
kybeen <rei980627@gmail.com>
2024-04-25 23:59:53 +0900

    [refactor] QuizViewModel 수정 - 뷰에서 필요한 Input/Output을 정의하고 이벤트에 대한 처리 및 결과를 관련 유즈케이스 스트림에 바인딩

--- PokemonTypeQuiz/Sources/Presentation/Quiz/QuizViewModel.swift
+++ PokemonTypeQuiz/Sources/Presentation/Quiz/QuizViewModel.swift
@@ -9,68 +9,118 @@ import Foundation
 import UIKit
 
 import RxSwift
+import RxCocoa
 import RxRelay
 
-final class QuizViewModel {
+protocol QuizViewModelType {
+    // input
+    var viewDidLoadEvent: PublishRelay<Void> { get }
+    var reloadButtonTap: PublishRelay<Void> { get }
+    var submitButtonTap: PublishRelay<Void> { get }
+    var typeCellTap: PublishRelay<Int> { get }
     
-    // 포켓몬 데이터가 변경될 때마다 받아오는 Observable
-    lazy var pokemonInfoObservable = BehaviorRelay<PokemonInfo>(value: .init(id: 0, koName: "none"))
-    
-    // 유저가 선택한 타입(인덱스) 배열
-    /// 컬렉션 뷰에서 선택한 타입 셀의 인덱스 번호를 가지고 있다가 해당 값을 이후 PokemonType 값으로 변환
-    var userTypeAnswer = [Int]()
-    
-    init() {
-    }
+    // output
+    var pokemonID: BehaviorRelay<String> { get }
+    var pokemonName: BehaviorRelay<String> { get }
+    var pokemonImage: BehaviorRelay<UIImage?> { get }
+    var pokemonType1: BehaviorRelay<PokemonType?> { get }
+    var pokemonType2: BehaviorRelay<PokemonType?> { get }
+    var userAnswer: BehaviorRelay<Set<Int>> { get }
+    var answerStatus: PublishRelay<AnswerStatus> { get }
 }
 
-// MARK: - 정답 처리
-
-extension QuizViewModel {
+final class QuizViewModel: QuizViewModelType {
     
-    // 답변 유형
-    enum AnswerType {
-        case correct(type1: PokemonType, type2: PokemonType?) // 정답
-        case noValue // 선택한 타입이 없음
-        case fail // 오답
+    // input
+    let viewDidLoadEvent = PublishRelay<Void>() // viewDidLoad 이벤트
+    let reloadButtonTap = PublishRelay<Void>() // 다시 불러오기 버튼 탭
+    let submitButtonTap = PublishRelay<Void>() // 정답 제출 버튼 탭
+    let typeCellTap = PublishRelay<Int>() // 타입 셀 선택 이벤트
+    
+    // output
+    let pokemonID = BehaviorRelay(value: "도감번호 : -")
+    let pokemonName = BehaviorRelay(value: "none")
+    let pokemonImage = BehaviorRelay<UIImage?>(value: nil)
+    let pokemonType1 = BehaviorRelay<PokemonType?>(value: nil)
+    let pokemonType2 = BehaviorRelay<PokemonType?>(value: nil)
+    let userAnswer = BehaviorRelay(value: Set<Int>())
+    let answerStatus = PublishRelay<AnswerStatus>()
+    
+    private let randomPokemonUseCase: RandomPokemonUseCase
+    private let answerUseCase: AnswerUseCase
+    
+    private let disposeBag = DisposeBag()
+    
+    init(_ randomPokemonUseCase: RandomPokemonUseCase,
+         _ answerUseCase: AnswerUseCase
+    ) {
+        self.randomPokemonUseCase = randomPokemonUseCase
+        self.answerUseCase = answerUseCase
+        
+        configureInput()
+        createOutput()
     }
     
-    // 정답 제출 버튼 클릭 시 메서드
-    func checkAnswer() -> AnswerType {
-        if userTypeAnswer.count == 0 { // 선택한 타입이 없는 경우
-            return .noValue
-        } else {
-            // 유저가 선택한 타입 값을 Int에서 PokemonType으로 변환
-            let answerArr = convertIndexToPokemonType(userTypeAnswer: userTypeAnswer)
-            if let type1Answer = pokemonInfoObservable.value.type1,
-               let type2Answer = pokemonInfoObservable.value.type2 { // 포켓몬의 타입이 2개일 때
-                // 둘 다 정답 배열에 있으면 정답
-                if answerArr.contains(type1Answer) && answerArr.contains(type2Answer) {
-                    return .correct(type1: type1Answer, type2: type2Answer)
-                } else {
-                    return .fail
-                }
-            } else { // 포켓몬의 타입이 1개일 때
-                if answerArr.count == 2 { // 타입을 2개 골랐으면 틀림
-                    return .fail
-                } else {
-                    if answerArr.contains(pokemonInfoObservable.value.type1!) {
-                        return .correct(type1: pokemonInfoObservable.value.type1!, type2: pokemonInfoObservable.value.type2)
-                    } else {
-                        return .fail
-                    }
-                }
-            }
-        }
+    private func configureInput() {
+        viewDidLoadEvent
+            .subscribe(onNext: { [weak self] in
+                self?.randomPokemonUseCase.loadRandomPokemon()
+            })
+            .disposed(by: disposeBag)
+        
+        reloadButtonTap
+            .subscribe(onNext: { [weak self] in
+                self?.randomPokemonUseCase.loadRandomPokemon()
+                self?.answerUseCase.resetUserAnswer()
+            })
+            .disposed(by: disposeBag)
+        
+        submitButtonTap
+            .subscribe(onNext: { [weak self] in
+                self?.answerUseCase.checkAnswer(type1: self?.pokemonType1.value, type2: self?.pokemonType2.value)
+            })
+            .disposed(by: disposeBag)
+        
+        typeCellTap
+            .subscribe(onNext: { [weak self] selectedCellIdx in
+                self?.answerUseCase.selectType(selectedCellIdx)
+            })
+            .disposed(by: disposeBag)
+        
     }
     
-    // 타입 인덱스가 들어 있는 정답 배열을 PokemonType 값이 들어 있는 배열로 바꿔서 반환하는 메서드
-    func convertIndexToPokemonType(userTypeAnswer: [Int]) -> [PokemonType] {
-        var pokemonTypeArr = [PokemonType]()
-        for idx in userTypeAnswer {
-            let value = PokemonType.allCases[idx]
-            pokemonTypeArr.append(value)
-        }
-        return pokemonTypeArr
+    private func createOutput() {
+        randomPokemonUseCase.pokemonInfo
+            .map { "도감번호 : \($0.id)" }
+            .bind(to: pokemonID)
+            .disposed(by: disposeBag)
+        
+        randomPokemonUseCase.pokemonInfo
+            .map { $0.koName }
+            .bind(to: pokemonName)
+            .disposed(by: disposeBag)
+        
+        randomPokemonUseCase.pokemonImageData
+            .map { UIImage(data: $0) }
+            .bind(to: pokemonImage)
+            .disposed(by: disposeBag)
+        
+        randomPokemonUseCase.pokemonInfo
+            .map { $0.type1 }
+            .bind(to: pokemonType1)
+            .disposed(by: disposeBag)
+        
+        randomPokemonUseCase.pokemonInfo
+            .map { $0.type2 }
+            .bind(to: pokemonType2)
+            .disposed(by: disposeBag)
+        
+        answerUseCase.userAnswer
+            .bind(to: userAnswer)
+            .disposed(by: disposeBag)
+        
+        answerUseCase.answerStatus
+            .bind(to: answerStatus)
+            .disposed(by: disposeBag)
     }
 }
